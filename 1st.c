ef66e5764001c5369064e518f5036d3f26d73a02
Akil Ashraful <akilashraful3@gmail.com>
2022-10-18 19:09:47 +0600

    first commit

---
+++ 1st.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (){
+
+    printf ("Energy conservation is the effort made to reduce the consumption of energy by using less of an energy service. This can be achieved either by using energy more efficiently or by reducing the amount of service used");
+
+}
