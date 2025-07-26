36c8a03adb378bbeeb8673d21859e899443c8516
akilashraful <akilashraful3@gmail.com>
2025-07-27 02:18:45 +0600

    Me

--- 1st.c
+++ 1st.c
@@ -4,6 +4,8 @@ int main (){
 
     printf ("Energy conservation is the effort made to reduce the consumption of energy by using less of an energy service. This can be achieved either by using energy more efficiently or by reducing the amount of service used");
 
-    printf ("halum")
+    printf ("halumMia")
+    printf ("where is my money")
+    printf ("halumMia")
 
 }
